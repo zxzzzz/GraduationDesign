841a7f8ce70977ae912324c91259949222edede4
19964162290zx <08143373@cumt.edu.cn>
2018-04-17 20:16:35 +0800

    v

--- judger/src/main/java/org/xm/judger/ApplicationTest.java
+++
@@ -1,8 +0,0 @@
-package org.xm.judger;
-import org.xm.xmnlp.Xmnlp;
-public class ApplicationTest {
-    public static void main(String[] args) {
-        String text="我是三年级二班的小熊同学，最近你过得怎么样呢？";
-        System.out.println(Xmnlp.segment(text));
-    }
-}
